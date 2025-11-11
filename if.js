0d3f9f5732a94c5186ea4e38f7729a4349f6b68f
rodasor <rodasor@gmail.com>
2025-11-11 13:30:12 -0300

    feat: comando if

---
+++ if.js
@@ -0,0 +1,20 @@
+let edad = 18;
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+// if/else if/else para múltiples condiciones
+let calificacion = 85;
+
+if (calificacion >= 90) {
+  console.log("Excelente");
+} else if (calificacion >= 80) {
+  console.log("Muy bien");
+} else if (calificacion >= 70) {
+  console.log("Bien");
+} else {
+  console.log("Necesitas mejorar");
+}
