dbb9233a337924e44f2a0c657eea4cee82809080
rodasor <rodasor@gmail.com>
2025-11-11 13:44:07 -0300

    feat: control switch

---
+++ switch.js
@@ -0,0 +1,30 @@
+let diaSemana = "lunes";
+
+switch (diaSemana) {
+  case "lunes":
+    console.log("Inicio de semana laboral");
+    break;
+  case "viernes":
+    console.log("¡Viernes!");
+    break;
+  case "sabado":
+  case "domingo":
+    console.log("Fin de semana");
+    break;
+  default:
+    console.log("Día de semana normal");
+}
+
+// switch con expresiones (ES6+)
+let calificacion = "A";
+
+switch (true) {
+  case calificacion === "A":
+    console.log("Excelente trabajo");
+    break;
+  case calificacion === "B":
+    console.log("Buen trabajo");
+    break;
+  default:
+    console.log("Sigue practicando");
+}
